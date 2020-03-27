f848ff40f8596ed1f574961572c5a9738e449eac
ninjasul <ninjasul@gmail.com>
2020-03-27 11:24:02 +0900

    JUnit5 - Basic Annotations

--- src/test/java/inflearn/whiteship/thejavatest/StudyTest.java
+++ src/test/java/inflearn/whiteship/thejavatest/StudyTest.java
@@ -1,7 +1,43 @@
 package inflearn.whiteship.thejavatest;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@Slf4j
 class StudyTest {
 
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        log.info("create");
+    }
+
+    @Test
+    @Disabled
+    void create1() {
+        log.info("create1");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        log.info("beforeAll");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        log.info("afterAll");
+    }
+
+    @BeforeEach
+    void setUp() {
+        log.info("beforeEach");
+    }
+
+    @AfterEach
+    void tearDown() {
+        log.info("afterEach");
+    }
 }
