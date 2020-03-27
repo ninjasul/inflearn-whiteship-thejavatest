8b7529ab7e0963f6d5f685e117923b20b7d8f1de
ninjasul <ninjasul@gmail.com>
2020-03-27 11:28:19 +0900

    JUnit5 - 테스트 이름 표기하기

--- src/test/java/inflearn/whiteship/thejavatest/StudyTest.java
+++ src/test/java/inflearn/whiteship/thejavatest/StudyTest.java
@@ -6,9 +6,11 @@ import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest {
 
     @Test
+    @DisplayName("스터디 만들기")
     void create() {
         Study study = new Study();
         assertNotNull(study);
@@ -17,6 +19,7 @@ class StudyTest {
 
     @Test
     @Disabled
+    @DisplayName("스터디 만들기1")
     void create1() {
         log.info("create1");
     }
