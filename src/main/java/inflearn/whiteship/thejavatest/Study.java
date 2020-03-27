5c9c6c155dbe269dad43c0e9c8b81f362fd1748a
1000141 <a1000141@1000141ui-MacBookPro.local>
2020-03-27 11:05:38 +0900

    Initial commit

---
+++ src/main/java/inflearn/whiteship/thejavatest/Study.java
@@ -0,0 +1,4 @@
+package inflearn.whiteship.thejavatest;
+
+public class Study {
+}
---
+++ src/test/java/inflearn/whiteship/thejavatest/StudyTest.java
@@ -0,0 +1,7 @@
+package inflearn.whiteship.thejavatest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+}
