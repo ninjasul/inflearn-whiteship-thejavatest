edaf1832f6ab6c490738278eb7e9d44a53b7426d
ninjasul <ninjasul@gmail.com>
2020-03-30 07:47:36 +0900

    JUnit5 - CustomTag

--- src/test/java/inflearn/whiteship/thejavatest/TagTest.java
+++ src/test/java/inflearn/whiteship/thejavatest/TagTest.java
@@ -2,21 +2,17 @@ package inflearn.whiteship.thejavatest;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
 
 @Slf4j
 public class TagTest implements BaseTest {
 
-    @Test
-    @Tag("A")
+    @TestA
     void test_tagA() {
         assertLimitIsGreaterThan0();
         log.info("test_tagA()");
     }
 
-    @Test
-    @Tag("B")
+    @TestB
     void test_tagB() {
         assertStudyIsNotNull();
         log.info("test_tagB()");
